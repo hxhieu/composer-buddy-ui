cb59f7bb8c9dcc4f4321e11c6a7ebe365192d15e
Hugh Hoang <hugh.hoang@gmail.com>
2020-08-13 22:48:18 +1200

    wip: home tiles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,43 @@
 <template>
-  <PageHeading>Home</PageHeading>
+  <div>
+    <PageHeading>Projects</PageHeading>
+    <div class="flex flex-wrap">
+      <ProjectTile empty />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ProjectTile from './HomeProjectTile.vue'
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    ProjectTile,
+  },
+})
+</script>
